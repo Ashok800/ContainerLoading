fe917ce8833537bb19f1fd9eaadb9fa9ff8eda23
Ashok800 <ashok.gorrela@imaginnovate.com>
2023-02-23 12:50:42 +0530

    Initial project setup

---
+++ src/main/java/com/hackathon/containerloading/ContainerLoadingApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.containerloading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerLoadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerLoadingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/containerloading/ContainerLoadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.containerloading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerLoadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
